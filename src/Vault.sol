41d7ff530cb9eaf954b6f85702ba1bc4f5608ecf
szabi-blockswap <szabi@blockswap.xyz>
2023-07-24 13:43:13 +0200

    Remappings && IERC20 replacement from openzeppelin

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import 'openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+
 contract Vault {
     IERC20 public immutable token;
 
@@ -60,23 +63,3 @@ contract Vault {
     }
 }
 
-interface IERC20 {
-    function totalSupply() external view returns (uint);
-
-    function balanceOf(address account) external view returns (uint);
-
-    function transfer(address recipient, uint amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint amount);
-    event Approval(address indexed owner, address indexed spender, uint amount);
-}
